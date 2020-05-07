93171c5f668845872e0a81a32b5434e9774c4fe2
surendhar9393 <62233779+surendhar9393@users.noreply.github.com>
2020-05-07 11:13:25 +0530

    Update admin.py

--- src/oscar/apps/partner/admin.py
+++ src/oscar/apps/partner/admin.py
@@ -4,12 +4,23 @@ from oscar.core.loading import get_model
 
 Partner = get_model('partner', 'Partner')
 StockRecord = get_model('partner', 'StockRecord')
+PartnerTaxInformation = get_model('customer', 'PartnerTaxInformation')
+
+
+class PartnerTaxInformationInline(admin.TabularInline):
+    model = PartnerTaxInformation
+    extra = 0
 
 
 class StockRecordAdmin(admin.ModelAdmin):
     list_display = ('product', 'partner', 'partner_sku', 'price_excl_tax', 'num_in_stock')
     list_filter = ('partner',)
+    
+
+class PartnerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'users', 'caption', 'product_classes', 'image', 'value_text')
+    inlines = [PartnerTaxInformationInline]
 
 
-admin.site.register(Partner)
+admin.site.register(Partner, PartnerAdmin)
 admin.site.register(StockRecord, StockRecordAdmin)
